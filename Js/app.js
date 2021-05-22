30783ae994dfc565b13dcbe1b9773fa64dc1dc55
SaraAli22 <sarasalem48@yahoo.com>
2021-05-22 23:18:28 +0300

    edit

--- Js/app.js
+++ Js/app.js
@@ -2,51 +2,73 @@
 
 let attempts = 0;
 let maxAttempts = 25;
+
 let attemptsEL = document.getElementById('attempts');
 
 let products = [];
-let productNames = [];
+let productImagesNames = [];
 let productsClicks = [];
 let productsViews = [];
-let lastPhoto =[];
 
-
+let lastPhoto = [];
 
 let leftImgEl = document.getElementById('leftImg');
+
 let middelImgEl = document.getElementById('middleImg');
 let rightImgEl = document.getElementById('rightImg');
 function ProductImg(productName) {
   this.productName = productName.split('.')[0];
-  this.Source = 'img/' + productName;
+  this.path = 'img/' + productName;
   this.clicks = 0;
   this.views = 0;
-
   products.push(this);
 
 }
 
-let productimgs = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg',
+
+
+function settingItems() {
+  let data = JSON.stringify(products);
+
+  localStorage.setItem('clicks and views', data);
+
+
+}
+
+
+
+
+function gettingItems() {
+  let stringEl = localStorage.getItem('clicks and views');
+  let normalEl = JSON.parse(stringEl);
+  if (normalEl !== null) {
+    products = normalEl;
+  }
+}
+
+
+
+let productImages = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg',
   'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg',
   'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'water-can.jpg', 'wine-glass.jpg'];
 
-
-
-for (let i = 0; i < productimgs.length; i++) {
-
-  new ProductImg(productimgs[i]);
+for (let i = 0; i < productImages.length; i++) {
+  new ProductImg(productImages[i]);
 }
 
+
+
 function randomImage() {
-  return Math.floor(Math.random() *productimgs.length);
+  return Math.floor(Math.random() * productImages.length);
 
 }
 
 
 let leftImage;
-
 let middelImage;
-
 let rightImage;
+
+
 
 function render() {
   leftImage = randomImage();
@@ -54,99 +76,86 @@ function render() {
   rightImage = randomImage();
 
   while (leftImage === middelImage || leftImage === rightImage || rightImage === middelImage || lastPhoto.includes(leftImage)
-  || lastPhoto.includes(middelImage) || lastPhoto.includes(rightImage)) {
+    || lastPhoto.includes(middelImage) || lastPhoto.includes(rightImage)) {
     leftImage = randomImage();
     middelImage = randomImage();
+    rightImage = randomImage();
   }
-
   leftImgEl.setAttribute('src', products[leftImage].path);
-
   leftImgEl.setAttribute('title', products[leftImage].path);
-
-
   products[leftImage].views++;
 
   middelImgEl.setAttribute('src', products[middelImage].path);
-
-
   middelImgEl.setAttribute('title', products[middelImage].path);
-
   products[middelImage].views++;
 
   rightImgEl.setAttribute('src', products[rightImage].path);
-
-
   rightImgEl.setAttribute('title', products[rightImage].path);
   products[rightImage].views++;
 
   attemptsEL.textContent = attempts;
-
   lastPhoto[0] = leftImage;
-
   lastPhoto[1] = middelImage;
-
   lastPhoto[2] = rightImage;
 
 }
 render();
 
 leftImgEl.addEventListener('click', handelClicks);
-
 middelImgEl.addEventListener('click', handelClicks);
-
 rightImgEl.addEventListener('click', handelClicks);
+let result1 = document.getElementById('results-containar');
+let button = document.createElement('button');
 
-let buttunEl = document.getElementById('button1');
-
-
+let ulEl = document.getElementById('results');
 function handelClicks(event) {
   attempts++;
-  if (attempts <= maxAttempts) {
 
+
+  if (attempts <= maxAttempts) {
     if (event.target.id === 'leftImg') {
       products[leftImage].clicks++;
 
     } else if (event.target.id === 'middleImg') {
       products[middelImage].clicks++;
-    } else if (event.target.id === 'rightImg') {
+    } else (event.target.id === 'rightImg');
+    {
       products[rightImage].clicks++;
     }
-
-
+    settingItems();
     render();
 
-
-
   } else {
+    leftImgEl.removeEventListener('click', handelClicks);
+    middelImgEl.removeEventListener('click', handelClicks);
+    rightImgEl.removeEventListener('click', handelClicks);
+
+
+    result1.appendChild(button1);
+    button1.textContent = 'click hear';
+    button1.addEventListener('click', result);
+
+   
     
+    function result(event) {
+      let liEl;
+      for (let i = 0; i < products.length; i++) {
+        liEl = document.createElement('li');
+        ulEl.appendChild(liEl);
+        liEl.textContent = `${products[i].productName} has ${products[i].views} views and has ${products[i].clicks} clicks.`;
+        productsClicks.push(products[i].clicks);
+        productsViews.push(products[i].views);
+        productImagesNames.push(products[i].productName);
+      }
+
+      //button1.removeEventListener('click', result);
+      chartRender();
+    }
   }
-
-
-
 }
-let ulEl = document.getElementById('results');
 
 
-function result() {
 
-  let liEl;
-  for (let i = 0; i < products.length; i++) {
-
-    liEl = document.createElement('li');
-    ulEl.appendChild(liEl);
-    liEl.textContent = `${products[i].productName} has ${products[i].views} views and has ${products[i].clicks} clicks.`;
-    productsClicks.push(products[i].clicks);
-    productsViews.push(products[i].views);
-    productNames.push(products[i].productName);
-  }
-  leftImgEl.removeEventListener('click', handelClicks);
-  middelImgEl.removeEventListener('click', handelClicks);
-  rightImgEl.removeEventListener('click', handelClicks);
-  chartRender();
-  console.log(productNames);
-  console.log(productsClicks);
-  console.log(productsViews);
-}
 
 function chartRender() {
   let ctx = document.getElementById('myChart').getContext('2d');
@@ -186,8 +195,7 @@ function chartRender() {
   });
 }
 
-console.log(productNames);
 
-console.log(productsClicks);
 
-console.log(productsViews);
+
+gettingItems();
