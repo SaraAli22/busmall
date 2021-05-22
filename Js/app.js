a376dfaf97a4470274ce6a6ed566e8cae69deee8
SaraAli22 <sarasalem48@yahoo.com>
2021-05-22 22:47:33 +0300

    Edit

--- Js/app.js
+++ Js/app.js
@@ -5,38 +5,47 @@ let maxAttempts = 25;
 let attemptsEL = document.getElementById('attempts');
 
 let products = [];
-let productImagesNames = [];
+let productNames = [];
 let productsClicks = [];
 let productsViews = [];
 let lastPhoto =[];
+
+
+
 let leftImgEl = document.getElementById('leftImg');
 let middelImgEl = document.getElementById('middleImg');
 let rightImgEl = document.getElementById('rightImg');
 function ProductImg(productName) {
   this.productName = productName.split('.')[0];
-  this.path = 'img/' + productName;
+  this.Source = 'img/' + productName;
   this.clicks = 0;
   this.views = 0;
+
   products.push(this);
 
 }
 
-let productImages = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg',
+let productimgs = ['bag.jpg', 'banana.jpg', 'bathroom.jpg', 'boots.jpg', 'breakfast.jpg', 'bubblegum.jpg',
   'chair.jpg', 'cthulhu.jpg', 'dog-duck.jpg', 'dragon.jpg', 'pen.jpg', 'pet-sweep.jpg', 'scissors.jpg',
   'shark.jpg', 'sweep.png', 'tauntaun.jpg', 'unicorn.jpg', 'water-can.jpg', 'wine-glass.jpg'];
 
-for (let i = 0; i < productImages.length; i++) {
-  new ProductImg(productImages[i]);
+
+
+for (let i = 0; i < productimgs.length; i++) {
+
+  new ProductImg(productimgs[i]);
 }
 
 function randomImage() {
-  return Math.floor(Math.random() * productImages.length);
+  return Math.floor(Math.random() *productimgs.length);
 
 }
 
 
 let leftImage;
+
 let middelImage;
+
 let rightImage;
 
 function render() {
@@ -49,34 +58,51 @@ function render() {
     leftImage = randomImage();
     middelImage = randomImage();
   }
+
   leftImgEl.setAttribute('src', products[leftImage].path);
+
   leftImgEl.setAttribute('title', products[leftImage].path);
+
+
   products[leftImage].views++;
 
   middelImgEl.setAttribute('src', products[middelImage].path);
+
+
   middelImgEl.setAttribute('title', products[middelImage].path);
+
   products[middelImage].views++;
 
   rightImgEl.setAttribute('src', products[rightImage].path);
+
+
   rightImgEl.setAttribute('title', products[rightImage].path);
   products[rightImage].views++;
 
   attemptsEL.textContent = attempts;
+
   lastPhoto[0] = leftImage;
+
   lastPhoto[1] = middelImage;
+
   lastPhoto[2] = rightImage;
 
 }
 render();
 
 leftImgEl.addEventListener('click', handelClicks);
+
 middelImgEl.addEventListener('click', handelClicks);
+
 rightImgEl.addEventListener('click', handelClicks);
+
 let buttunEl = document.getElementById('button1');
+
 
 function handelClicks(event) {
   attempts++;
   if (attempts <= maxAttempts) {
+
     if (event.target.id === 'leftImg') {
       products[leftImage].clicks++;
 
@@ -85,6 +111,8 @@ function handelClicks(event) {
     } else if (event.target.id === 'rightImg') {
       products[rightImage].clicks++;
     }
+
+
     render();
 
 
@@ -103,18 +131,19 @@ function result() {
 
   let liEl;
   for (let i = 0; i < products.length; i++) {
+
     liEl = document.createElement('li');
     ulEl.appendChild(liEl);
     liEl.textContent = `${products[i].productName} has ${products[i].views} views and has ${products[i].clicks} clicks.`;
     productsClicks.push(products[i].clicks);
     productsViews.push(products[i].views);
-    productImagesNames.push(products[i].productName);
+    productNames.push(products[i].productName);
   }
   leftImgEl.removeEventListener('click', handelClicks);
   middelImgEl.removeEventListener('click', handelClicks);
   rightImgEl.removeEventListener('click', handelClicks);
   chartRender();
-  console.log(productImagesNames);
+  console.log(productNames);
   console.log(productsClicks);
   console.log(productsViews);
 }
@@ -157,6 +186,8 @@ function chartRender() {
   });
 }
 
-console.log(productImagesNames);
+console.log(productNames);
+
 console.log(productsClicks);
+
 console.log(productsViews);
